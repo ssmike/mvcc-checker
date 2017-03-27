efde98c429c16f4949760b0a1703a3f1b912a01f
Mike <surinmike@gmail.com>
2017-03-27 11:48:39 +0300

    initial dyntables support

--- src/jepsen/dyntables.clj
+++ src/jepsen/dyntables.clj
@@ -1,5 +1,6 @@
 (ns jepsen.dyntables
   (:gen-class)
+  (:import knossos.model.Model)
   (:require [clojure.tools.logging :refer :all]
             [clojure.java.io    :as io]
             [clojure.string     :as str]
@@ -13,22 +14,27 @@
                     [tests      :as tests]
                     [util       :refer [timeout]]]
             [jepsen.os.debian   :as debian]
-            [knossos.model      :as model]))
+            [knossos.model      :as model]
+            [jepsen.yt          :as yt]
+            [knossos.model :as knossos]))
+
+(def inconsistent knossos/inconsistent)
 
 (def db
   (reify db/DB
     (setup! [_ test node]
       (c/su
-        (c/exec :bash "/master/run.sh")
+        (c/exec :bash "/jepsen/run.sh")
         (info (str node " set up"))))
     (teardown! [_ test node]
       (c/su
-        (c/exec :bash "/master/stop.sh")
+        (c/exec :bash "/jepsen/stop.sh")
         (info (str node " teardown!"))))
     db/LogFiles
       (log-files [_ test node]
-        ["/master/master.debug.log" "/master/master.log"])))
-
+        (case node
+          :master ["/master/master.debug.log" "/master/master.log"]
+          ["/node/node.debug.log" "/node/node.log"]))))
 
 (defn client
   [con]
@@ -36,41 +42,84 @@
     (setup! [this test node]
         (info "waiting for yt")
         (let [sock (yt/start-client)]
-          (info "waiting for yt")
-          (yt/ysend sock {:f :wait-for-yt})
+          (info "waiting for master")
+          (yt/wait-master sock)
+          (info "mounting dyn-table")
+          (yt/verify-table-mounted sock)
           (info "yt proxy set up")
           (client sock)))
     (invoke! [this test op]
-      (timeout 5000 (assoc op :type :info, :error :timeout)
+      (timeout 3000 (assoc op :type :info, :error :timeout)
         (merge op (yt/ysend con op))))
     (teardown! [_ test] (yt/close con))))
 
-(defn r-gen   [_ _] {:type :invoke, :f :read, :value nil})
-(defn w-gen   [_ _] {:type :invoke, :f :write, :value (rand-int 20000)})
+(defrecord Dict [dict]
+  Model
+  (step [m op]
+    (let [op-val (:value op)
+          {key "key" val "val"} op-val]
+      (case (:f op)
+        :dyn-table-read (do
+                          (assert key (str op))
+                          (if (= (get dict key) val)
+                          m
+                          (inconsistent (str "can't read " val " with key " key))))
+        :dyn-table-write (Dict. (assoc dict key val))
+        :dyn-table-cas  (let [[from-key to-key] key
+                              [from-val to-val] val]
+                              (assert to-val (str op val))
+                              (assert to-key (str op key))
+                              (assert from-key (str op key))
+                              (if (contains? dict from-key)
+                                (let [res (mod (+ to-val (get dict from-key)) 5)]
+                                  (Dict. (assoc dict to-key res)))
+                                (inconsistent (str "can't cas with " from-key))))))))
+
+(defn empty-dict
+  []
+  (Dict. {}))
+
+(def yt-keys
+  {0 2
+   1 21
+   2 31})
+
+(defn rand-key
+  []
+  (get yt-keys (rand-int 3)))
+
+(defn r-gen   [_ _] {:type :invoke :f :dyn-table-read
+                     :value {"key" (rand-key)}})
+(defn w-gen   [_ _] {:type :invoke :f :dyn-table-write
+                     :value {"val" (rand-int 5) :key (rand-key)}})
+(defn cas-gen [_ _] {:type :invoke :f :dyn-table-cas
+                     :value {"val" [(rand-int 5) (rand-int 5)]
+                             "key" [(rand-key) (rand-key)]}})
 
 
-(defn c-test
+(defn d-test
   "Given an options map from the command-line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
   (info "Creating test" opts)
   (merge tests/noop-test
          opts
-         {:name     "Cypress"
+         {:nodes   [:n1 :n2 :n3 :n4 :n5 :master]
+          :name    "Dyntables"
           :os      debian/os
           :db      db
           :client  (client nil)
           :nemesis (nemesis/partition-random-halves)
-          :timeout 200
-          :generator (->> (gen/mix [r-gen w-gen])
+          :timeout 60
+          :generator (->> (gen/mix [r-gen w-gen cas-gen cas-gen])
                           (gen/stagger 1)
                           (gen/nemesis
                             (gen/seq (cycle [(gen/sleep 9)
                                              {:type :info, :f :start}
                                              (gen/sleep 9)
                                              {:type :info, :f :stop}])))
-                          (gen/time-limit 190))
-          :model   (model/register 0)
+                          (gen/time-limit 60))
+          :model   (empty-dict)
           :checker (checker/compose
                      {:perf   (checker/perf)
                       :linear checker/linearizable})
@@ -82,6 +131,6 @@
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
   [& args]
-  (cli/run! (merge (cli/single-test-cmd {:test-fn c-test})
+  (cli/run! (merge (cli/single-test-cmd {:test-fn d-test})
                    (cli/serve-cmd))
- 
+            args))
---
+++ src/jepsen/yt.clj
@@ -0,0 +1,85 @@
+(ns jepsen.yt
+  (:gen-class)
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [clojure.java.shell :as sh])
+  [:import java.lang.Runtime])
+
+(defn encode
+  [op]
+  (let [allowed-keys #{:f :value :req-id}]
+    (json/write-str
+      (conj {} (filter (fn [[x _]]
+                         (contains? allowed-keys x))
+                       op)))))
+
+(defn decode
+  [msg]
+  (let [mp (json/read-str msg)]
+    (into {} (map (fn [[a b]] [(keyword a)
+                               ((case a
+                                  ("value" "req-id" "key")
+                                    identity
+                                    keyword)
+                                 b)])
+                  mp))))
+
+(defn ysend
+  [{:keys [in cache req-cnt]} msg]
+  (let [result (promise)
+        id (swap! req-cnt inc)]
+    (swap! cache (fn [mp] (assoc mp id result)))
+    (binding [*out* in]
+      (println (encode (assoc msg :req-id id)))
+      (flush))
+    (let [value @result]
+      (swap! cache (fn [mp] (dissoc mp id)))
+      value)))
+
+(defn close
+  [{:keys [reader in] :as client}]
+  (ysend client {:f :terminate})
+  (future-cancel reader))
+
+(def proxy-num (atom 0))
+
+(defn start-client
+  []
+  (let [cnt (swap! proxy-num inc)
+        proc (. (Runtime/getRuntime) exec (into-array ["run-proxy.sh" (str cnt)]))
+        cache (atom {})
+        out (io/reader (. proc getInputStream))
+        worker (fn []
+                (binding [*in* out]
+                  (loop []
+                    (let [{id :req-id :as msg} (decode (read-line))]
+                      (deliver (get @cache id) msg))
+                    (recur))))]
+    {:in (io/writer (. proc getOutputStream))
+     :cache cache
+     :req-cnt (atom 0)
+     :proxy-id cnt
+     :reader (future (worker))}))
+
+(def table-mounted
+  [(ref false)
+   (ref nil)])
+
+(defn verify-table-mounted
+  [sock]
+  (let [[mounted mount-result] table-mounted
+        my-promise (promise)]
+    (let [[im-first to-deliver]
+          (dosync
+              (if (not @mounted)
+                (do (ref-set mounted true)
+                    (ref-set mount-result my-promise)
+                 [true my-promise])
+                [false @mount-result]))]
+      (if im-first (do (deliver to-deliver (ysend sock {:f :mount-table}))
+                       @to-deliver)
+                   @to-deliver))))
+
+(defn wait-master
+  [sock]
+  (ysend sock {:f :wait-for-yt}))
