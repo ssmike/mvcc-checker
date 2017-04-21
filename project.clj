99f09bf46d47350b8b76f543dfdf84980a066733
Mike <surinmike@gmail.com>
2017-04-21 11:18:36 +0300

    tune jvm options

--- project.clj
+++ project.clj
@@ -8,4 +8,15 @@
                  [org.clojure/data.json "0.2.6"]]
   :main jepsen.dyntables
   :target-path "target/%s"
+  :jvm-opts ["-Xmx50g"
+             "-XX:+UseConcMarkSweepGC"
+             "-XX:+UseParNewGC"
+             "-XX:+CMSParallelRemarkEnabled"
+             "-XX:+AggressiveOpts"
+             "-XX:+UseFastAccessorMethods"
+             "-XX:MaxInlineLevel=32"
+             "-XX:MaxRecursiveInlineLevel=2"
+             "-XX:+UnlockCommercialFeatures"
+;             "-XX:-OmitStackTraceInFastThrow"
+             "-server"]
   :profiles {:uberjar {:aot :all}})
