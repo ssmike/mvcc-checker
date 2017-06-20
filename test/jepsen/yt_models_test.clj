64686f1e1e2275d4490edab3da271557640c3467
Mike <surinmike@gmail.com>
2017-06-20 19:34:23 +0300

    improve tests

--- test/jepsen/yt_models_test.clj
+++ test/jepsen/yt_models_test.clj
@@ -12,99 +12,145 @@
             [knossos [model :as model]]
             [unilog.config :as unilog]))
 
-(defn set-process
-  ([history]
-    (set-process history 1))
+(def ^:dynamic operation-cache)
 
-  ([history proc]
-    (map #(assoc % :process proc) history)))
+(defmacro hist-gen
+  [& body]
+  `(binding [operation-cache (atom (transient{}))]
+     (list ~@body)))
 
-(def one-line-success
-  (set-process
-    [{:type :invoke :f :read-and-lock :value [0 nil]}
-     {:type :ok :f :read-and-lock :value [0 1]}
-     {:type :invoke :f :write-and-unlock :value [2 2]}
-     {:type :ok :f :write-and-unlock :value [2 2]}
-     {:type :invoke :f :read-and-lock :value [1 nil]}
-     {:type :ok :f :read-and-lock :value [1 1]}
-     {:type :invoke :f :write-and-unlock :value [0 0]}
-     {:type :ok :f :write-and-unlock :value [0 0]}]))
+(defn read-op[proc key val]
+  (let [op {:type :invoke :f :read-and-lock :value [key val] :process proc}]
+    (swap! operation-cache assoc! proc op)
+    (assoc op :value [key nil])))
 
-(def one-line-read-failure
-  (set-process
-    [{:type :invoke :f :read-and-lock :value [0 nil]}
-     {:type :ok :f :read-and-lock :value [0 1]}
-     {:type :invoke :f :write-and-unlock :value [2 2]}
-     {:type :ok :f :write-and-unlock :value [2 2]}
-     {:type :invoke :f :read-and-lock :value [1 nil]}
-     {:type :fail :f :read-and-lock :value [1 nil]}
-     {:type :invoke :f :write-and-unlock :value [0 0]}
-     {:type :fail :f :write-and-unlock :value [0 0]}]))
+(defn write-op[proc key val]
+  (let [op {:type :invoke :f :write-and-unlock :value [key val] :process proc}]
+    (swap! operation-cache assoc! proc op)
+    op))
 
-(def one-line-write-failure
-  (set-process
-    [{:type :invoke :f :read-and-lock :value [0 nil]}
-     {:type :ok :f :read-and-lock :value [0 1]}
-     {:type :invoke :f :write-and-unlock :value [2 2]}
-     {:type :ok :f :write-and-unlock :value [2 2]}
-     {:type :invoke :f :read-and-lock :value [1 nil]}
-     {:type :ok :f :read-and-lock :value [1 1]}
-     {:type :invoke :f :write-and-unlock :value [0 0]}
-     {:type :info :f :write-and-unlock :value [0 0]}]))
+(defn op-fail[proc]
+  (let [op (@operation-cache proc)]
+    (swap! operation-cache dissoc! proc)
+    (assoc op :type :fail)))
+
+(defn op-hangs[proc]
+  (let [op (@operation-cache proc)]
+    (swap! operation-cache dissoc! proc)
+    (assoc op :type :info)))
+
+(defn op-ok[proc]
+  (let [op (@operation-cache proc)]
+    (swap! operation-cache dissoc! proc)
+    (assoc op :type :ok)))
+
+(defmacro test-line
+  [history]
+  `(let [history# (->> ~history
+                       foldup-locks
+                       complete-history)
+         m# (memo/memo yt/empty-locked-dict history#)
+         result# (wgl/check
+                   (:init m#)
+                   (:history m#)
+                   (:edges m#))]
+     (is (:valid? result#) "according to checker history is invalid")))
+
+(defn one-line-success
+  []
+  (hist-gen
+    (read-op :1 0 1)
+    (op-ok :1)
+    (write-op :1 2 2)
+    (op-ok :1)
+    (read-op :1 1 1)
+    (op-ok :1)
+    (write-op :1 0 0)
+    (op-ok :1)))
+
+(deftest one-line-success-correctness
+  (test-line (one-line-success)))
+
+(defn one-line-read-failure
+  []
+  (hist-gen
+    (read-op :1 0 1)
+    (op-ok :1)
+    (write-op :1 2 2)
+    (op-ok :1)
+    (read-op :1 1 1)
+    (op-fail :1)
+    (write-op :1 0 0)
+    (op-fail :1)))
+
+(deftest one-line-read-failure-correctness
+  (test-line (one-line-read-failure)))
+
+(defn one-line-write-failure
+  []
+  (hist-gen
+    (read-op :1 0 1)
+    (op-ok :1)
+    (write-op :1 2 2)
+    (op-ok :1)
+    (read-op :1 1 1)
+    (op-ok :1)
+    (write-op :1 0 0)
+    (op-hangs :1)))
+
+(deftest one-line-write-failure-correctness
+  (test-line (one-line-write-failure)))
 
 (defn pretty-print
   [list]
   (run! println list))
 
-;; just ensure that history generators don't throw exceptions
-(deftest one-line-history-test
-  (run! (fn [hist] (-> hist
-                       foldup-locks
-                       complete-history
-                       pretty-print)
-                   (println))
-       [one-line-success
-        one-line-read-failure
-        one-line-write-failure]))
+(defn unacknowledged-commit
+  []
+  (hist-gen
+    (read-op :1 0 1)
+    (op-ok :1)
+    (write-op :1 1 2)
+    (read-op :2 1 2)
+    (op-hangs :1)
+    (op-ok :2)
+    (write-op :2 0 2)
+    (op-fail :2)))
 
-(defmacro test-line
-  [history]
-  `(->> ~history
-        foldup-locks
-        (linear/analysis yt/empty-locked-dict)))
+(deftest unacknowledged-commit-test
+  (test-line (unacknowledged-commit)))
 
-(deftest memo-test
-  (run! (fn [hist] (let [m (->> hist
-                                foldup-locks
-                                complete-history
-                                (memo/memo yt/empty-locked-dict))]
-                     (is (:valid?
-                           (wgl/check
-                             (:init m)
-                             (:history m)
-                             (:edges m))))))
-       [one-line-success
-        one-line-read-failure
-        one-line-write-failure]))
+(defn timed-out-commit
+  []
+  (hist-gen
+    (read-op :1 0 1)
+    (op-ok :1)
+    (write-op :1 1 2)
+    (read-op :2 1 1)
+    (op-hangs :1)
+    (op-ok :2)
+    (write-op :2 0 2)
+    (op-fail :2)))
+
+(deftest timed-out-commit-test
+  (test-line (timed-out-commit)))
 
 (defn checkfile
   [fname]
   (let [history  (->> fname
                       slurp
-                      read-string
-                      foldup-locks
-                      complete-history)
-        m (memo/memo yt/empty-locked-dict history)]
-    (wgl/check (:init m) (:history m) (:edges m))))
+                      read-string)]
+    (test-line history)))
 
 (deftest check-logs
   (if (.exists (java.io.File. "checker-test"))
-    (is (:valid?
-          (checkfile "checker-test")))
-    (error "file doesn't exist")))
+    (checkfile "checker-test")
+    (do
+      (error "can't read checker-test")
+      (info "pwd is" (System/getProperty "user.dir")))))
 
 (defn without-debug[f]
   (unilog/start-logging! {:console true :level "info"})
   (f))
 
-;(use-fixtures :once without-debug)
+(use-fixtures :once without-debug)
