ca0d2ddb1697ffec15261e6821a1ff2f4817bb31
Mike <surinmike@gmail.com>
2017-06-22 20:40:00 +0300

    introduce concurrent strategy

--- src/jepsen/dyntables/wgl/strategy.clj
+++ src/jepsen/dyntables/wgl/strategy.clj
@@ -43,3 +43,58 @@
             (recur (into stack to-insert)))))))
 
 (defn depth-first[] (DepthFirst. ()))
+
+(deftype Concurrent[threads local-cache]
+  IExecutionStrategy
+  (schedule [_ fun]
+    (swap! local-cache conj fun)
+    nil)
+
+  (execute! [_]
+    (let [search-success (atom false)
+          queue (atom '(nil ()))
+          broadcaster (fn[]
+                        (let [[fetched _] (swap! queue
+                                                 (fn [[_ q]]
+                                                   (list (first q)
+                                                         (rest q))))]
+                          (or fetched
+                              (do
+                                (debug "worker idle")
+                                (Thread/sleep 200)
+                                ()))))
+
+          granularity (* threads 3)
+
+          worker-fn (fn [cache]
+                      (when-not @search-success
+                         (doseq [item (broadcaster)]
+                           (debug "running item")
+                           (if @item (reset! search-success true)))
+                         (doseq [batch (partition-all granularity (reverse @cache))]
+                           (swap! queue (fn [[_ q]]
+                                          (list nil (conj q batch)))))
+                         (reset! cache ())
+                         (recur cache)))
+
+          workers (for [_ (range threads)]
+                    (future
+                      (let [cache (atom ())]
+                        (binding [*current-strategy* (Concurrent. nil cache)]
+                          (worker-fn cache)))))]
+      (reset! queue (list nil (list @local-cache)))
+      (reset! local-cache nil) ; to ensure that nobody touches parent context
+      (debug "running workers")
+      (doall workers)
+      ; join them
+      (run! deref workers)
+      @search-success)))
+
+(defn concurrent[threads] (Concurrent. threads (atom ())))
+
+(defn factory
+  [opts]
+  (case (:execution-strategy opts)
+    :concurrent (concurrent (:threads opts))
+    :recursive (recursion)
+    :dfs (depth-first)))
