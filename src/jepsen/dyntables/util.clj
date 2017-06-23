7c6f68a0140967531337103455fc3b1afc51eef3
Mike <surinmike@gmail.com>
2017-06-23 12:00:44 +0300

    don't fail on empty histories

--- src/jepsen/dyntables/util.clj
+++ src/jepsen/dyntables/util.clj
@@ -1,18 +1,22 @@
-(ns jepsen.dyntables.util)
+(ns jepsen.dyntables.util
+  (:require [clojure.tools.logging :refer [debug info warn]]))
 
 (def infinity 1e9)
 
 (defn transitions-count
   [history]
-  (->> history 
-       (map :value)
-       (apply concat)
-       (apply max)
-       (+ 1)))
+  (if (empty? history)
+    (do (warn "There were 0 successful transitions. Seems suspicious.") 0)
+    (->> history
+         (map :value)
+         (apply concat)
+         (apply max)
+         (+ 1))))
 
 (defn models-count
   [edges]
   (->> edges
        (mapcat (fn [it] [(it 0) (it 2)]))
+       (cons 0)
        (apply max)
        (+ 1)))
